b98cb3794547124b332ad4a7c1068f75541d87df
KSA8182019 <48059633+KSA8182019@users.noreply.github.com>
2019-03-23 20:58:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,10 @@
- const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '-'
- 
+ const prefix = '-'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Daily | Server`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+client.user.setActivity('', { type: 'WATCHING' })
 });
-
 
 
 
@@ -45,6 +22,9 @@ client.on("message", message => {
  message.delete(); 
 };     
 });
+
+
+
 
 
 
